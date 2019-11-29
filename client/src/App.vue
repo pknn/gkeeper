d5959897cdbcb1ea9026c786fd70d3b6ca3465aa
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-11-30 00:54:37 +0700

    Add clientside code

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="postcss">
+@import "assets/styles/tailwind.postcss";
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,600|Solway:400,700&display=swap");
+
+.text-primary {
+  font-family: "Solway", serif;
+}
+
+.text-secondary {
+  font-family: "Montserrat", sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply text-primary;
+}
+
+a,
+p,
+span,
+div,
+body {
+  @apply text-secondary;
+}
+</style>
---
+++ client/src/views/Splash.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex flex-col justify-center items-center bg-white">
+    <img
+      class="logo"
+      :class="{ wooshIn: beginWooshIn, wooshOut: beginWooshOut }"
+      src="https://image.flaticon.com/icons/png/512/628/628283.png"
+      alt="logo"
+    />
+    <h1 class="brand-name" :class="{ wooshOut: beginWooshOut }">
+      Green Keeper
+    </h1>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.logo {
+  @apply p-4 rounded-full;
+  transition: transform 800ms ease-out;
+  float: left;
+  height: 300px;
+  overflow: hidden;
+  &.wooshIn {
+    transform: scale(6);
+  }
+  &.wooshOut {
+    transform: scale(0.01);
+  }
+}
+
+.brand-name {
+  @apply m-2 text-4xl text-app-main;
+  &.wooshOut {
+    display: none;
+  }
+}
+
+.button {
+  @apply text-lg px-10 py-3 m-2 font-bold rounded-full shadow-md;
+  &.primary {
+    @apply bg-app-accent text-gray-100;
+  }
+  &.secondary {
+    @apply bg-gray-100;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "splash",
+  data: () => ({
+    beginWooshIn: false,
+    beginWooshOut: false
+  }),
+  mounted() {
+    setTimeout(() => {
+      this.beginWooshIn = true;
+    }, 1000);
+    setTimeout(() => {
+      this.beginWooshOut = true;
+    }, 2700);
+    setTimeout(() => {
+      this.$router.push({ name: "home" });
+    }, 3500);
+  }
+};
+</script>
